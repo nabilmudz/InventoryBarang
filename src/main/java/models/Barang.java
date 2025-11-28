603761fac66e8a453405c00c783291c5ab1634eb
Jett <fawwazna26@gmail.com>
2025-11-28 14:59:17 +0700

    Update barang

--- src/main/java/models/Barang.java
+++ src/main/java/models/Barang.java
@@ -1,1 +1,62 @@
 package models;
+
+import java.time.LocalDateTime;
+
+public class Barang {
+
+    private int id;
+    private String nama;
+    private int stok;
+    private double harga;
+    private int supplierId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Barang() {}
+
+    public Barang(int id, String nama, int stok, double harga, int supplierId,
+                   LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.nama = nama;
+        this.stok = stok;
+        this.harga = harga;
+        this.supplierId = supplierId;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    // GETTER & SETTER
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNama() { return nama; }
+    public void setNama(String nama) { this.nama = nama; }
+
+    public int getStok() { return stok; }
+    public void setStok(int stok) { this.stok = stok; }
+
+    public double getHarga() { return harga; }
+    public void setHarga(double harga) { this.harga = harga; }
+
+    public int getSupplierId() { return supplierId; }
+    public void setSupplierId(int supplierId) { this.supplierId = supplierId; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+
+    public LocalDateTime getUpdatedAt() { return updatedAt; }
+    public void setUpdatedAt(LocalDateTime updatedAt) { this.updatedAt = updatedAt; }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", nama='" + nama + '\'' +
+                ", stok=" + stok +
+                ", harga=" + harga +
+                ", supplierId=" + supplierId +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
